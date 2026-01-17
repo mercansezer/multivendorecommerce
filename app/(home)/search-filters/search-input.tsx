4d2b3bc0881ce595760e7673dbebfac17e8288d9
Sezer Mercan <sezermercan12@hotmail.com>
2026-01-18 01:26:25 +0300

    06-fixing bug

--- app/(home)/search-filters/search-input.tsx
+++ app/(home)/search-filters/search-input.tsx
@@ -1,12 +1,26 @@
+"use client";
 import { Input } from "@/components/ui/input";
-import { SearchIcon } from "lucide-react";
+import { ListFilterIcon, SearchIcon } from "lucide-react";
+import { Category } from "../types";
+import React from "react";
+import { Categories } from "./categories";
+import CategoriesSideBar from "./CategoriesSideBar";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 interface Props {
   disabled?: boolean;
+  data: Category[];
 }
-export function SearchInput({ disabled }: Props) {
+export function SearchInput({ disabled, data }: Props) {
+  const [sideBarOpen, setSideBarOpen] = React.useState(true);
   return (
     <div className="flex items-center gap-2 w-full">
+      <CategoriesSideBar
+        data={data}
+        open={sideBarOpen}
+        onOpenChange={setSideBarOpen}
+      />
       <div className="relative w-full">
         <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
         <Input
@@ -15,6 +29,13 @@ export function SearchInput({ disabled }: Props) {
           disabled={disabled}
         />
       </div>
+      <Button
+        variant="elevated"
+        className={cn("lg:hidden hover:bg-white border-primary  text-black ")}
+        onClick={() => setSideBarOpen(true)}
+      >
+        <ListFilterIcon className="size-4" />
+      </Button>
     </div>
   );
 }
